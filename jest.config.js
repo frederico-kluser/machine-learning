d14bb1daa1af133c3b0b42acaf9f1862494ab625
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-12 09:13:09 -0300

    feat: force min jest coverage to 100

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,26 @@
 export default {
-  testEnvironment: 'node',
-  preset: 'ts-jest/presets/default-esm',
-  transform: {
-    '^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    'src/**/*.mts',
-    '!src/**/*.d.ts',
-    '!src/**/*.d.mts',
-  ],
+    testEnvironment: 'node',
+    preset: 'ts-jest/presets/default-esm',
+    transform: {
+        '^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        'src/**/*.mts',
+        '!src/**/*.d.ts',
+        '!src/**/*.d.mts',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
 };
